05240c1b30b99de490eefb66351cf6654b16e72e
Spoder <madowdney@cincinnatistate.edu>
2024-09-18 14:33:41 -0400

    Add files via upload

---
+++ Assignment4_MD/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.lblNumber = New System.Windows.Forms.Label()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(59, 16)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumber.TabIndex = 0
+        '
+        'lblNumber
+        '
+        Me.lblNumber.AutoSize = True
+        Me.lblNumber.Location = New System.Drawing.Point(9, 19)
+        Me.lblNumber.Name = "lblNumber"
+        Me.lblNumber.Size = New System.Drawing.Size(44, 13)
+        Me.lblNumber.TabIndex = 1
+        Me.lblNumber.Text = "Number"
+        '
+        'btnOk
+        '
+        Me.btnOk.Location = New System.Drawing.Point(48, 71)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(75, 23)
+        Me.btnOk.TabIndex = 2
+        Me.btnOk.Text = "Ok"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(182, 118)
+        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.lblNumber)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Name = "Form1"
+        Me.Text = "Sum of numbers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNumber As TextBox
+    Friend WithEvents lblNumber As Label
+    Friend WithEvents btnOk As Button
+End Class
---
+++ Assignment4_MD/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        Dim intSum As Double
+        Dim i = 1
+        Dim intNumber As Double
+        Dim blnValidated As Boolean = True
+
+        Call Get_Validate_Int_Input(intNumber, blnValidated)
+
+        If blnValidated = True Then
+            ' While the input number is greater than the sum, add to the sum
+            Do While i <= txtNumber.Text
+                intSum += i
+                i += 1
+            Loop
+            MessageBox.Show("The sum is " + intSum.ToString())
+
+        End If
+
+
+    End Sub
+    Private Sub Get_Validate_Int_Input(ByRef intNumber As String, ByRef blnValidated As Boolean)
+        ' Validate the number input
+        If Integer.TryParse(txtNumber.Text, intNumber) Then
+
+            If intNumber < 1 Then
+                MessageBox.Show("Number must be greater than 0")
+                blnValidated = False
+                txtNumber.Focus()
+
+            End If
+        Else
+            ' Input is not a valid integer
+            MessageBox.Show("Number must exist and be numeric")
+            blnValidated = False
+            txtNumber.Focus()
+        End If
+
+    End Sub
+End Class
